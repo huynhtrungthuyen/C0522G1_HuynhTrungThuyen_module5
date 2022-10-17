a11d398abc28b38751485d69a8960f120519f36a
huynhtrungthuyen <huynhtrungthuyen@gmail.com>
2022-10-17 16:27:32 +0700

    case 4

--- 03_angular_overview/exercise/angular_calculator_app/src/app/calculator/calculator.component.ts
+++ 03_angular_overview/exercise/angular_calculator_app/src/app/calculator/calculator.component.ts
@@ -8,7 +8,7 @@ import {Component, OnInit} from '@angular/core';
 export class CalculatorComponent implements OnInit {
   num1 = 0;
   num2 = 0;
-  result: any = "";
+  result: any;
 
   constructor() {
   }
---
+++ angular-case-study/src/app/facility/facility.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-facility',
+  templateUrl: './facility.component.html',
+  styles: []
+})
+export class FacilityComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
