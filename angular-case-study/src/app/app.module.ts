c11575993d0d7e84486cbd9e62a938062bd076f9
huynhtrungthuyen <huynhtrungthuyen@gmail.com>
2022-10-19 14:00:30 +0700

    case 2.9

--- angular-case-study/src/app/app.module.ts
+++ angular-case-study/src/app/app.module.ts
@@ -10,6 +10,11 @@ import { HomeComponent } from './home/home.component';
 import { FacilityComponent } from './facility/facility.component';
 import { EditFacilityComponent } from './edit-facility/edit-facility.component';
 import { AddFacilityComponent } from './add-facility/add-facility.component';
+import { CustomerComponent } from './customer/customer.component';
+import { EditCustomerComponent } from './edit-customer/edit-customer.component';
+import { AddCustomerComponent } from './add-customer/add-customer.component';
+import { AddContractComponent } from './add-contract/add-contract.component';
+import { ContractComponent } from './contract/contract.component';
 
 @NgModule({
   declarations: [
@@ -19,7 +24,12 @@ import { AddFacilityComponent } from './add-facility/add-facility.component';
     HomeComponent,
     FacilityComponent,
     EditFacilityComponent,
-    AddFacilityComponent
+    AddFacilityComponent,
+    CustomerComponent,
+    EditCustomerComponent,
+    AddCustomerComponent,
+    AddContractComponent,
+    ContractComponent
   ],
   imports: [
     BrowserModule,
