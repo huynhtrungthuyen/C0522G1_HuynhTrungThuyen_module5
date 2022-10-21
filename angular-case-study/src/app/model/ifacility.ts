0c0f3f6674cdbc4b3cdd7628a279e79fe994cee6
huynhtrungthuyen <huynhtrungthuyen@gmail.com>
2022-10-21 21:00:19 +0700

    case 3.1

---
+++ angular-case-study/src/app/model/ifacility.ts
@@ -0,0 +1,14 @@
+export interface IFacility {
+  facilityId?: string;
+  facilityName?: string;
+  facilityArea?: number;
+  rentCost?: number;
+  maxPeople?: number;
+  standardRoom?: string;
+  descriptionOtherConvenience?: string;
+  poolArea?: number;
+  numberOfFloors?: number;
+  facilityFree?: string;
+  rentType?: string;
+  facilityType?: string;
+}
