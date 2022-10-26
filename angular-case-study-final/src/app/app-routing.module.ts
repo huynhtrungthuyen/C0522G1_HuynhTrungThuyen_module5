0bf273968c194c89472f9aeda9fd7640b05b4639
huynhtrungthuyen <huynhtrungthuyen@gmail.com>
2022-10-26 14:56:52 +0700

    bt final

--- angular-case-study-final/src/app/app-routing.module.ts
+++ angular-case-study-final/src/app/app-routing.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {HomeComponent} from './home/home.component';
+import {FacilityModule} from './facility/facility.module';
+import {CustomerModule} from './customer/customer.module';
+import {ContractModule} from './contract/contract.module';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'facility', loadChildren: () => import('./facility/facility.module').then(module => module.FacilityModule)},
   {path: 'customer', loadChildren: () => import('./customer/customer.module').then(module => module.CustomerModule)},
   {path: 'contract', loadChildren: () => import('./contract/contract.module').then(module => module.ContractModule)}
+  // {path: 'facility', loadChildren: () => FacilityModule},
+  // {path: 'customer', loadChildren: () => CustomerModule},
+  // {path: 'contract', loadChildren: () => ContractModule}
 ];
 
 @NgModule({
